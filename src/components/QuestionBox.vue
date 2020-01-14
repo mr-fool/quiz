ff2434136aafdeeb57de83832ead40e61311a533
mr-fool <mr-fool@users.noreply.github.com>
2020-01-14 00:49:06 -0700

    fixing question box

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -8,8 +8,8 @@
 
         <hr class="my-4">
 
-        <p>
-            List of answers
+        <p v-for="(answer, index) in answers" :key="index">
+            {{answer}}
         </p>
 
         <b-button variant="primary" href="#">Submit</b-button>
@@ -23,6 +23,16 @@
         props: {
             currentQuestion: Object,
             next: Function
+        },
+        computed: {
+            answers() {
+                let answers = [...this.currentQuestion.incorrect_answers];
+                answers.push(this.currentQuestion.correct_answer);
+                return answers;
+            }
+        },
+        mounted(){
+            console.log(this.currentQuestion)
         }
     }
 
