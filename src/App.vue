c026b1ba5a8ef7226c19cd8cf9f03d616640151a
mr-fool <mr-fool@users.noreply.github.com>
2020-01-15 22:59:42 -0700

    fixing small bugs

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
-<div id="app">
-    <Header/>
+  <div id="app">
+    <Header
+      :numCorrect="numCorrect"
+      :numTotal="numTotal"
+    />
+
     <b-container class="bv-example-row">
-        <b-row>
-            <b-col sm="6" offset="3">
-                <QuestionBox
-                    v-if="questions.length"
-                    :currentQuestion="questions[index]"
-                    :next="next"
-                />
-            </b-col>
-
-        </b-row>
+      <b-row>
+        <b-col sm="6" offset="3">
+          <QuestionBox
+            v-if="questions.length"
+            :currentQuestion="questions[index]"
+            :next="next"
+            :increment="increment"
+          />
+        </b-col>
+      </b-row>
     </b-container>
-
   </div>
 </template>
 
@@ -28,15 +31,23 @@ export default {
     QuestionBox
   },
   data() {
-      return {
-          questions: [],
-          index: 0
-      }
+    return {
+      questions: [],
+      index: 0,
+      numCorrect: 0,
+      numTotal: 0
+    }
   },
   methods: {
-      next() {
-          this.index++
+    next() {
+      this.index++
+    },
+    increment(isCorrect) {
+      if (isCorrect) {
+        this.numCorrect++
       }
+      this.numTotal++
+    }
   },
   mounted: function() {
     fetch('https://opentdb.com/api.php?amount=10&category=31&type=multiple', {
